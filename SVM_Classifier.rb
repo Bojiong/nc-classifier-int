6e22e5bac9411cd2182c815dc069c61264927675
Mike Z <mjz48@cornell.edu>
2012-04-01 17:33:50 -0400

    mangled the code, sorry

--- SVM_Classifier.rb
+++ SVM_Classifier.rb
@@ -1,76 +1,93 @@
 ##
 # SVM_Classifier module
 #
-# instance: svm_thresold, svm_model, svm_order
-# private method: convert_to_svm(hash)
-# public method: svm_approve(hash)
+#
+#
+# instance:
+#   svm_thresold, svm_model, svm_order
+#
+# public methods:
+#   svm_approve(hash)
+#
+# private methods:
+#   convert_to_svm(hash)
 #
 # bn82. Mar,29,2012
+# mjz48. Apr,01,2012
 #
-module SVM_Classifier
+class SVM_Classifier
 
 # instances
 attr_accessor :svm_threshold, :svm_model, :svm_order
 
 # constructor
-def initialize(svm_threshold, svm_model, svm_order)
-	@svm_threshold = svm_threshold
-	@svm_model = svm_model
-	@svm_order = svm_order
+  def initialize(params = {})
+    self.svm_threshold = params[:svm_threshold]
+    self.svm_model = params[:svm_model]
+    self.svm_order = params[:svm_order]
+  end
+
+  #
+  # @parameters - hash of user values
+  # @return - [boolean, float]
+  #
+  def approve(hash = {})
+
+    # some defines... >_<
+    #svm_command = "./svm_classify"  # for linux/mac
+    svm_command = "exec/svm_classify.exe" # for windows
+    predict_file = "result_tmp.txt"
+
+    # convert to svm format and write to file
+    user_input = convert_to_svm(hash)
+
+    # run the svm classifier
+    system "#{svm_command} -v 0 #{user_input} #{self.svm_model} #{predict_file}"
+
+    # read result
+    file = File.open(predict_file, "r")
+    line = file.gets
+    predict = line.split(" ")[0].to_f
+
+    # delet tmp files
+    file.close
+    File.delete(predict_file)
+    File.delete(user_input)
+
+    soft = predict - svm_threshold
+    if soft > 0
+      return [TRUE, soft]
+    else
+      return [FALSE, soft]
+    end
+  end
+
+  private
+
+  #
+  # @parameter: hash of user values
+  # @return: name of the created svm file
+  #
+  def convert_to_svm(hash = {})
+    user_name = hash[:name]
+    file_name = "#{user_name}_input.svm"
+    user_file = File.new(file_name, "w")
+    user_file.write("0 ")
+
+    len = svm_order.length
+    i = 0
+    while i < len
+      begin
+        param = hash[svm_order[i]].to_f
+      rescue
+        param = 0.0                  # default 0?????
+      end
+      i += 1
+      user_file.write("#{i}:#{param} ")
+    end
+
+    user_file.write("\n")
+    user_file.close()
+    return file_name
+  end
 end
-
-#
-# @parameters - hash of user values
-# @return - [boolean, float]
-#
-def svm_approve(hash)
-	# convert to svm
-	user_input = convert_to_svm(hash)
-	# run svm
-	predict_file = "result_tmp.txt"
-	system "./svm_classify -v 0 #{user_input} #{@svm_model} #{predict_file}"
-	# read result
-	file = File.open(predict_file, "r")
-	line = file.gets
-	predict  = line.split(" ")[0].to_f
-	# delete rmp files
-	file.close
-	File.delete(predict_file)
-	File.delete(user_input)
-	soft = predict - svm_threshold
-	if soft > 0
-		return [TRUE, soft]
-	else return [FALSE, soft]
-	end
-end
-
-private
-
-#
-# @parameter: hash of user values
-# @return: name of the created svm file 
-#
-def convert_to_svm(hash)
-	user_name = hash[:name]
-	file_name = "#{user_name}_input.svm"
-	user_file = File.new(file_name, "w")
-	user_file.write("0 ")
-	
-	len = svm_order.length
-	i = 0
-	while i < len
-		begin
-			param = hash[svm_order[i]].to_f
-			rescue
-				param = 0.0                  # default 0?????
-		end
-		i += 1
-		user_file.write("#{i}:#{param} ")
-	end
-	user_file.write("\n")
-	user_file.close
-	return file_name
-end
-
-end
-
---
+++ SVM_Classifier.test.rb
@@ -0,0 +1,80 @@
+##
+# SVM_Classifier unit test
+#
+# mjz48. Apr,01,2012
+
+require 'NC_Classifier.rb'
+require 'SVM_Classifier.rb'
+
+puts ""
+puts "======================"
+puts "SVM_Classifier.test.rb"
+
+tfilename = "tdata/SVM_Classifier.test.data"
+
+order = [:RevolvingUtilizationOfUnsecuredLines,
+    :age,:NumberOfTime3059DaysPastDueNotWorse,:DebtRatio,
+    :MonthlyIncome,:NumberOfOpenCreditLinesAndLoans,
+    :NumberOfTimes90DaysLate,:NumberRealEstateLoansOrLines,
+    :NumberOfTime6089DaysPastDueNotWorse,:NumberOfDependents]
+
+puts "NC_Classifier Object created."
+nc_classifier = SVM_Classifier.new({
+                  :svm_model => "models/model_2_0.1_0.1",
+                  :svm_threshold => 0,
+                  :svm_order => order })
+
+#
+# used for debug convert_to_svm
+# To use:
+#   comment out line 47 "private" in file SVM_Classifier.rb
+# delete "=begin" and "=end" below
+#
+=begin
+user_input = c.convert_to_svm(hash,order)
+
+file = File.open(user_input, "r")
+puts file.gets
+=end
+
+# get testcases
+tfile = File.open(tfilename, "r")
+
+tc_num = 1
+line = tfile.gets()
+while line != nil
+  line = line.split(" ")
+  user_input = {}
+
+  # parse the result
+  testcase_result = (line[0] == "1") ? true : false
+
+  # parse the user input
+  user_input[:SeriousDlqin2yrs] = "0"
+  user_input[:name] = line[1]
+  user_input[:RevolvingUtilizationOfUnsecuredLines] = line[2]
+  user_input[:age] = line[3]
+  user_input[:NumberOfTime3059DaysPastDueNotWorse] = line[4]
+  user_input[:DebtRatio] = line[5]
+  user_input[:MonthlyIncome] = line[6]
+  user_input[:NumberOfOpenCreditLinesAndLoans] = line[7]
+  user_input[:NumberOfTimes90DaysLate] = line[8]
+  user_input[:NumberRealEstateLoansOrLines] = line[9]
+  user_input[:NumberOfTime6089DaysPastDueNotWorse] = line[10]
+  user_input[:NumberOfDependents] = line[11]
+
+  classifier_result = nc_classifier.approve(user_input)
+
+  print "TC#{tc_num} :: expected: #{testcase_result}, "
+  print "actual: (#{classifier_result[0]}, #{classifier_result[1]})"
+  if classifier_result[0] != testcase_result
+    print "  <== Misclassified!"
+  end
+  puts ""
+
+  # get new data
+  line = tfile.gets()
+  tc_num += 1
+end
+
+puts "Done."
